cfb3ac71c663053b47ac25bc7d3cf0828247b4b8
Vincent Loubiere <vincent.loubiere@hotmail.fr>
2020-11-27 23:28:08 +0100

    new analysis home computer

--- A_transcriptomes_processing.R
+++ A_transcriptomes_processing.R
@@ -7,7 +7,7 @@ require(data.table)
 # Build dm6 index
 #----------------------------------------------------------#
 # setwd("/_R_data/genomes/dm6/subreadr_index/")
-# ref <- "/home/vloubiere/genomes/dm6/Sequence/WholeGenomeFasta/genome.fa"
+# ref <- "/_R_data/genomes/dm6/Sequence/WholeGenomeFasta/genome.fa"
 # buildindex(basename= "subreadr_dm6_index", reference= ref)
 # setwd("/_R_data/projects/epigenetic_cancer/")
 
