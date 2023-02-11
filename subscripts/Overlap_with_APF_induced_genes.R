fe9f605a9450ac9aba3b1b7f52fd6cd9f372fec7
vloubiere <vincent.loubiere@imp.ac.at>
2023-02-11 12:11:04 +0100

    released for review

--- subscripts/Overlap_with_APF_induced_genes.R
+++
@@ -1,8 +0,0 @@
-setwd("/mnt/d/_R_data/projects/epigenetic_cancer/")
-require(vlfunctions)
-require(data.table)
-
-WPP <- as.data.table(read_excel("external_data/APF-6h_vs_APF+6h_doi10.1073pnas.1900343116_s1.xlsx"))
-dat <- fread("Rdata/RECOVERY_NORECOVERY_genes.txt")
-fisher.test(dat$PHD9_RECOVERY, dat$symbol %in% WPP[behavior=="WT_+6hAPF_higher", Gene.ID], alternative = "greater")
-fisher.test(dat$PHD9_RECOVERY, dat$symbol %in% WPP[behavior=="WT_-6hAPF_higher", Gene.ID], alternative = "greater")
